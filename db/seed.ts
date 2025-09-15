6f3a3b05a45c47aa5baec2833a7b54b9def94b63
phanlan99 <youremail@example.com>
2025-09-15 10:36:47 +0700

    04-seed-sample-data

--- db/seed.ts
+++ db/seed.ts
@@ -1,30 +1,16 @@
 import { PrismaClient } from '@prisma/client';
 import sampleData from './sample-data';
-import { hash } from '@/lib/encrypt';
 
 async function main() {
   const prisma = new PrismaClient();
+
+  // Xóa hết các sản phẩm hiện có trong database
   await prisma.product.deleteMany();
-  await prisma.account.deleteMany();
-  await prisma.session.deleteMany();
-  await prisma.verificationToken.deleteMany();
-  await prisma.user.deleteMany();
 
+  // Thêm các sản phẩm từ dữ liệu mẫu
   await prisma.product.createMany({ data: sampleData.products });
-  const users = [];
-  for (let i = 0; i < sampleData.users.length; i++) {
-    users.push({
-      ...sampleData.users[i],
-      password: await hash(sampleData.users[i].password),
-    });
-    console.log(
-      sampleData.users[i].password,
-      await hash(sampleData.users[i].password)
-    );
-  }
-  await prisma.user.createMany({ data: users });
 
-  console.log('Database seeded successfully!');
+  console.log('Database seeded successfully');
 }
 
 main();
